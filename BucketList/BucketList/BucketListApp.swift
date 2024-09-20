1e9e1b27b023489181910c3882bb44bc23b67bee
galih <samudragalih94@gmail.com>
2024-09-20 13:33:56 +0700

    wip initial push

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Galih Samudra on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
