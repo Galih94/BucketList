b426ce6b165e66dc73b749dcffb49dc666b46159
galih <samudragalih94@gmail.com>
2024-09-23 12:54:42 +0700

    clean code

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -4,32 +4,11 @@
 //
 //  Created by Galih Samudra on 20/09/24.
 //
-
 import SwiftUI
 
-struct User: Identifiable {
-    let id = UUID()
-    var firstName: String
-    var lastName: String
-}
-
-extension User: Comparable {
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let values = [1,5,3,6,2,9].sorted()
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Dave", lastName: "Jones"),
-        User(firstName: "Jess", lastName: "Donner")
-    ].sorted()
     var body: some View {
-        List(users) { user in
-            Text(String(user.firstName + " " + user.lastName))
-        }
+        Text("Hello world")
     }
 }
 
