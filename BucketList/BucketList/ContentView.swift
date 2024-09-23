aa7e103fe3304742d9ffab95350eaf98ab084dc7
galih <samudragalih94@gmail.com>
2024-09-23 11:15:55 +0700

    example(List): use Array of `Identifiable` objects

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,11 +7,22 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
     let values = [1,5,3,6,2,9].sorted()
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Dave", lastName: "Jones"),
+        User(firstName: "Jess", lastName: "Donner")
+    ]
     var body: some View {
-        List(values, id: \.self) { value in
-            Text(String(value))
+        List(users) { user in
+            Text(String(user.firstName + " " + user.lastName))
         }
     }
 }
