6243ec98161238be47942f9ea1d652df3209d837
galih <samudragalih94@gmail.com>
2024-09-23 11:35:02 +0700

    example(Comparable): move sort logic to object User by make User conforn to `Comparable`

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -13,15 +13,19 @@ struct User: Identifiable {
     var lastName: String
 }
 
+extension User: Comparable {
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
     let values = [1,5,3,6,2,9].sorted()
     let users = [
         User(firstName: "Arnold", lastName: "Rimmer"),
         User(firstName: "Dave", lastName: "Jones"),
         User(firstName: "Jess", lastName: "Donner")
-    ].sorted {
-        $0.lastName < $1.lastName
-    }
+    ].sorted()
     var body: some View {
         List(users) { user in
             Text(String(user.firstName + " " + user.lastName))
