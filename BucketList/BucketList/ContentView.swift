4ca39797626f53bd4d95d8c3e4b576ceb50a6945
galih <samudragalih94@gmail.com>
2024-09-23 13:28:49 +0700

    clean code

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,26 +8,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Read and Write") {
-            let data = Data("Test message".utf8)
-            FileManager().write(data, path: "message.txt")
-        }
+        Text("Hello world")
     }
 }
 
 #Preview {
     ContentView()
-}
-
-extension FileManager {
-    func write(_ data: Data, path: String) {
-        let url = URL.documentsDirectory.appending(path: path)
-        do {
-            try data.write(to: url, options: [.atomic, .completeFileProtection])
-            let input = try String(contentsOf: url)
-            print("input -- \(input)")
-        } catch {
-            print("error -- \(error.localizedDescription)")
-        }
-    }
 }
