8e5ea113a8196dc5648b5ab24373df0c3c421e90
galih <samudragalih94@gmail.com>
2024-09-23 13:09:14 +0700

    example(URL): example to write data on document directory

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,7 +8,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello world")
+        Button("Read and Write") {
+            let data = Data("Test message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print("error -- \(error.localizedDescription)")
+            }
+        }
     }
 }
 
