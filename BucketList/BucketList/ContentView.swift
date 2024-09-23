8ecf082fff8117cddd4184961705bd0cab80b81b
galih <samudragalih94@gmail.com>
2024-09-23 14:48:38 +0700

    feat(Enum): apply enum in control UI state

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,9 +6,39 @@
 //
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Text("Hello world")
+        Text("Loading")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success")
+    }
+}
+struct FailedView: View {
+    var body: some View {
+        Text("Failed")
+    }
+}
+
+
+struct ContentView: View {
+    @State private var loadingState: LoadingState = .loading
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
     }
 }
 
