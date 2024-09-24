03a8c41ad30c27391470ee0d4fe224dabeacc6d8
galih <samudragalih94@gmail.com>
2024-09-24 11:15:47 +0700

    clean code

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -4,83 +4,11 @@
 //
 //  Created by Galih Samudra on 20/09/24.
 //
-import MapKit
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222,
-                                                          longitude: -0.1275),
-                           span: MKCoordinateSpan(latitudeDelta: 1,
-                                                  longitudeDelta: 1)
-        )
-    )
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     var body: some View {
-        MapReader { proxy in
-            Map(position: $position) {
-                ForEach(locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Text(location.name)
-                            .font(.headline)
-                            .padding()
-                            .background(.blue)
-                            .foregroundStyle(.white)
-                            .clipShape(.capsule)
-                    }
-                    .annotationTitles(.hidden)
-                }
-            }
-            .mapStyle(.hybrid(elevation: .realistic))
-            .onMapCameraChange(frequency: .continuous) { context in
-                
-                print("map: \(context.region)")
-            }
-            .onTapGesture { tapPosition in
-                if let coordinate = proxy.convert(tapPosition, from: .local) {
-                    print("tap coordinate: \(coordinate)")
-                }
-            }
-        }
-        HStack {
-            Button("Paris") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
-            }
-            
-            Button("Tokyo") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
-            }
-            
-            Button("London") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222,
-                                                                      longitude: -0.1275),
-                                       span: MKCoordinateSpan(latitudeDelta: 1,
-                                                              longitudeDelta: 1)
-                    )
-                )
-            }
-        }
+        Text("Hello world")
     }
 }
 
