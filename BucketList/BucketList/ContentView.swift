fab43427fcd60ed6706583f110278f976b0a4f8c
galih <samudragalih94@gmail.com>
2024-09-24 09:57:19 +0700

    clean code

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,39 +6,9 @@
 //
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success")
-    }
-}
-struct FailedView: View {
-    var body: some View {
-        Text("Failed")
-    }
-}
-
-
 struct ContentView: View {
-    @State private var loadingState: LoadingState = .loading
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
+        Text("Hello world")
     }
 }
 
