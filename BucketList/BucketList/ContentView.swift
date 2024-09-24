8a6a2e12a9e23696c3b99625115bb74612b050c2
galih <samudragalih94@gmail.com>
2024-09-24 13:12:27 +0700

    feat(Biometric): implement biometric auth

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -4,11 +4,50 @@
 //
 //  Created by Galih Samudra on 20/09/24.
 //
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello world")
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: {
+            authenticate()
+        })
+        
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        /// check is biometric authentication possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            /// exist biometrics
+            let reason = "We need to unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { isSuccess, authError in
+                
+                /// completed authentication
+                if isSuccess {
+                    print("success")
+                    isUnlocked = true
+                } else {
+                    print("error -- \(authError?.localizedDescription)")
+                    isUnlocked = false
+                }
+            }
+        } else {
+            /// no biometrics
+            print("error -- no biometrics")
+        }
     }
 }
 
