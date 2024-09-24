e75827e06d3c3177eca145aa3197cdfa80ca531f
galih <samudragalih94@gmail.com>
2024-09-24 10:22:30 +0700

    feat(Map): set position programmatically on tap Button

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -18,6 +18,35 @@ struct ContentView: View {
     var body: some View {
         Map(position: $position)
             .mapStyle(.hybrid(elevation: .realistic))
+        HStack {
+            Button("Paris") {
+                position = MapCameraPosition.region(
+                    MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                    )
+                )
+            }
+            
+            Button("Tokyo") {
+                position = MapCameraPosition.region(
+                    MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                    )
+                )
+            }
+            
+            Button("London") {
+                position = MapCameraPosition.region(
+                    MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222,
+                                                                      longitude: -0.1275),
+                                       span: MKCoordinateSpan(latitudeDelta: 1,
+                                                              longitudeDelta: 1)
+                    )
+                )
+            }
+        }
     }
 }
 
