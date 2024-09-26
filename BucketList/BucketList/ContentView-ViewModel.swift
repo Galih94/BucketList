1dd90a3ec3a61445b37ec13e09d0f136918e4e63
galih <samudragalih94@gmail.com>
2024-09-26 11:34:55 +0700

    feat(Local): save location to local memory

--- BucketList/BucketList/ContentView-ViewModel.swift
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -13,18 +13,38 @@ extension ContentView {
     @Observable
     class ViewModel {
         let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
-        private(set) var locations = [Location]()
+        private(set) var locations: [Location]
         var selectedPlace: Location?
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
         
         func addLocation(at point: CLLocationCoordinate2D) {
             let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: point.latitude, longitude: point.longitude)
             locations.append(newLocation)
+            save()
         }
         
         func update(location: Location) {
             guard let selectedPlace else { return }
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("error -- \(error.localizedDescription)")
             }
         }
     }
